5a8e384191a8bb834b9c8516acabdf1e2079f3b6
yuto1174 <82192131+yuto1174@users.noreply.github.com>
2022-02-14 19:09:00 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,11 @@
-<input type="button" onclick="location.href='https://pure-lake-58882.herokuapp.com/home.php'" value="new">
+
+<!DOCTYPE html>
+<html dir="ltr" lang="ja">
+<meta charset="UTF-8">
+<html>
+<head><title>PHP TEST</title></head>
+<body>
+  <input type="button" onclick="location.href='https://pure-lake-58882.herokuapp.com/home.php'" value="new">
+  
+  </body>
+</html>
